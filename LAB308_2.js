e78efe2b8a7d8affec515b76aaf4d4591f296a2d
Tracey-goode <Jerand322@gmail.com>
2025-07-31 23:56:25 -0400

    Lab commit for 308.2

---
+++ LAB308_2.js
@@ -0,0 +1,38 @@
+// Control Flow
+
+let r = 5
+const pi = 3.1415
+let p = 20
+const sp = 0.8
+
+//plants double every week
+
+// Week 1
+//Pruned
+let area = (pi * (r * r))
+console.log(area) //78.54
+console.log(area * .8) //62.83
+let minSp = (area / 16)
+console.log(minSp)
+
+
+
+if (minSp > area) {
+    console.log(`Prune`); {
+    } if (minSp < area) {
+        console.log(`No Pruning`);
+    }
+    if ((minSp > area) > (.5 * area)); {
+        console.log(`monitored`); {
+        }
+    }
+}
+//Monitered
+
+
+
+//Planted
+
+
+
+//Week 2
